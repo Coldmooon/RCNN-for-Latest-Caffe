9b1c0e0daa275ceefc4e539dc487172a853e3eba
coldmooon <liy@bupt.edu.cn>
2019-03-16 17:26:33 +0800

    update pool5_tofcx for latest caffe

--- rcnn_pool5_to_fcX.m
+++ rcnn_pool5_to_fcX.m
@@ -18,7 +18,8 @@ if layer > 5
   for i = 6:layer
     % weights{1} = matrix of CNN weights [input_dim x output_dim]
     % weights{2} = column vector of biases
-    feat = max(0, bsxfun(@plus, feat*rcnn_model.cnn.layers(i).weights{1}, ...
-                          rcnn_model.cnn.layers(i).weights{2}'));
+    layerid = ['fc', num2str(i)];
+    feat = max(0, bsxfun(@plus, feat*rcnn_model.cnn.model.layers(layerid).params(1).get_data(), ...
+                      rcnn_model.cnn.model.layers(layerid).params(2).get_data()') );
   end
 end
