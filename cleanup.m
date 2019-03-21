3b5afeca169ce989393c14217eb0cbf3e735eda3
coldmooon <liy@bupt.edu.cn>
2019-03-21 10:50:34 +0800

    formatted cleanup.m outputs

--- cleanup.m
+++ cleanup.m
@@ -10,18 +10,18 @@ switch cache
     delete('./imdb/cache/imdb_voc_2007_test.mat');
     delete('./cachedir/voc_2007_test/*');
     delete('./datasets/VOCdevkit2007/local/VOC2007/test_anno.mat');
-    fprintf('done!')
+    fprintf('done!\n')
   case 'train'
     % clean train cache  
     delete('./imdb/cache/imdb_voc_2007_train.mat');
     delete('./imdb/cache/imdb_voc_2007_trainval.mat');
     delete('./cachedir/voc_2007_train/*');
     delete('./cachedir/voc_2007_trainval/*');
-    fprintf('done!')
+    fprintf('done!\n')
   case 'feature'
     % clean feature cache  
     delete('./feat_cache/v1_finetune_voc_2007_trainval_iter_70k/*')
-    fprintf('done!')
+    fprintf('done!\n')
   otherwise
     error('unknown objective ''%s'' [valid options: ''cache'' or ''train'' or ''feature'']', cache);
 end     
